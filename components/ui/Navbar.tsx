eeecf2bc7fee235e4fafaaf8d1bc99a94019de7b
Karl Behrens <karlbehrensg@gmail.com>
2022-04-17 18:33:27 -0400

    Redirect to Home onClick navbar Title

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
+import NextLink from "next/link";
 import Image from "next/image";
-import { Spacer, Text, useTheme } from "@nextui-org/react";
+import { Link, Spacer, Text, useTheme } from "@nextui-org/react";
 
 export const Navbar = () => {
   const { theme } = useTheme();
@@ -23,12 +24,16 @@ export const Navbar = () => {
         height={70}
       />
 
-      <Text color="white" h2>
-        P
-      </Text>
-      <Text color="white" h3>
-        okemon
-      </Text>
+      <NextLink href="/" passHref>
+        <Link>
+          <Text color="white" h2>
+            P
+          </Text>
+          <Text color="white" h3>
+            okemon
+          </Text>
+        </Link>
+      </NextLink>
 
       <Spacer
         css={{
