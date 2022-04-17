a61f3bb381c18a910dc3f4f78dca1fcdffae8998
Karl Behrens <karlbehrensg@gmail.com>
2022-04-17 14:00:24 -0400

    Set _document.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
 
+import { Button } from '@nextui-org/react'
+
 const HomePage: NextPage = () => {
   return (
     <>
-      <h1>Hola mundo</h1>
+      <Button color="gradient">Hola Mundo</Button>
     </>
   )
 }
