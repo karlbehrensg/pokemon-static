de76b11b8771aece8950fbd83242d02f128d72d3
Karl Behrens <karlbehrensg@gmail.com>
2022-04-17 15:59:09 -0400

    Set type in axios response

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { GetStaticProps, NextPage } from "next";
 import { pokeApi } from "../api";
 import { Layout } from "../components/layouts";
+import { PokemonListResponse } from "../interfaces";
 
 const HomePage: NextPage = (props) => {
   console.log({ props });
@@ -22,7 +23,7 @@ const HomePage: NextPage = (props) => {
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const { data } = await pokeApi.get("/pokemon?limit=151");
+  const { data } = await pokeApi.get<PokemonListResponse>("/pokemon?limit=151");
 
   return {
     props: {
