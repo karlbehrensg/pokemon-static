2b65e13e8d4cabff3344f5c5756e8b10aaf297c7
Karl Behrens <karlbehrensg@gmail.com>
2022-04-18 13:56:12 -0400

    Meta tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -6,6 +6,8 @@ interface Props {
   title?: string;
 }
 
+const origin = typeof window === "undefined" ? "" : window.location.origin;
+
 export const Layout: FC<PropsWithChildren<Props>> = ({ children, title }) => {
   return (
     <>
@@ -17,6 +19,15 @@ export const Layout: FC<PropsWithChildren<Props>> = ({ children, title }) => {
           content={`Information about pokemon ${title}`}
         />
         <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+        <meta
+          property="og:title"
+          content={`Information about pokemon ${title}`}
+        />
+        <meta
+          property="og:description"
+          content={`This page contains information about pokemon ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
       </Head>
 
       <Navbar />
