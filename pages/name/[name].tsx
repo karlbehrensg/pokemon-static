89c3d76f5b82f66c68c29eca3562edfffdd04720
Karl Behrens <karlbehrensg@gmail.com>
2022-04-18 13:39:04 -0400

    Centralize data reader

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -6,8 +6,8 @@ import confetti from "canvas-confetti";
 
 import { pokeApi } from "../../api";
 import { Layout } from "../../components/layouts";
-import { Pokemon, PokemonListResponse } from "../../interfaces";
-import { localFavorites } from "../../utils";
+import { PokemonListResponse } from "../../interfaces";
+import { getPokemonInfo, localFavorites } from "../../utils";
 
 interface Props {
   pokemon: any;
@@ -117,17 +117,10 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { name } = params as { name: string };
-  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${name}`);
-
-  const pokemon = {
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites,
-  };
 
   return {
     props: {
-      pokemon,
+      pokemon: await getPokemonInfo(name),
     },
   };
 };
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -4,10 +4,8 @@ import { Button, Card, Container, Grid, Text, Image } from "@nextui-org/react";
 
 import confetti from "canvas-confetti";
 
-import { pokeApi } from "../../api";
 import { Layout } from "../../components/layouts";
-import { Pokemon } from "../../interfaces";
-import { localFavorites } from "../../utils";
+import { localFavorites, getPokemonInfo } from "../../utils";
 
 interface Props {
   pokemon: any;
@@ -116,17 +114,10 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
-  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${id}`);
-
-  const pokemon = {
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites,
-  };
 
   return {
     props: {
-      pokemon,
+      pokemon: await getPokemonInfo(id),
     },
   };
 };
